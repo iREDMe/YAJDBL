fcddba6133072d3d55371670ee253dee2342d089
PixelTheGreat <42497408+PixelTheGreat@users.noreply.github.com>
2018-11-25 16:06:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-module.exports =
 {
     Client: require('./Client/Client'),
     MessageEmbed: require('./Structures/MessageEmbed'),
+        
+     // Util
+    Collection: require('./Util/Collection'),
     version: require('../package.json').version,
 };
